c62510aa49ca4a5211076700553fd174d0ee672a
David <kisswen660@163.com>
2025-09-14 23:06:28 +0800

    Update chrome_app.rs
    fix: regedit install chrome extension app

--- src-tauri/src/api/chrome_app.rs
+++ src-tauri/src/api/chrome_app.rs
@@ -3,17 +3,25 @@ use tauri::Result;
 use winreg::RegKey;
 use winreg::enums::HKEY_LOCAL_MACHINE;
 
-#[tauri::command]
-pub fn add_extensions(id: &str) -> Result<serde_json::Value> {
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let cur_ver = hklm.open_subkey("SOFTWARE\\WOW6432Node\\Google\\Chrome")?;
-    let extensions = match cur_ver.open_subkey("Extensions") {
+fn create_subkey (key: &str, cur_ver: &RegKey)-> Result<RegKey>{
+      let sub_key = match cur_ver.open_subkey(key) {
         Ok(subkey) => subkey,
         Err(_) => {
-            let (subkey, _) = cur_ver.create_subkey("Extensions")?;
+            let (subkey, _) = cur_ver.create_subkey(&key)?;
             subkey
         }
     };
+    Ok(sub_key)
+}
+
+
+#[tauri::command]
+pub fn add_extensions(id: &str) -> Result<serde_json::Value> {
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let cur_ver = hklm.open_subkey("SOFTWARE\\WOW6432Node")?;
+    let cur_ver = create_subkey("Google", &cur_ver)?;
+    let cur_ver = create_subkey("Chrome", &cur_ver)?;
+    let extensions = create_subkey("Extensions", &cur_ver)?;
     let (id_ver, _) = extensions.create_subkey(id)?;
     id_ver.set_value(
         "update_url",
